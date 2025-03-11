896d9ea6de2a53abc6ec56b47f9283579462901a
sakil-sagor <sakilsagor76543@gmail.com>
2025-03-12 01:45:55 +0600

    update

--- controllers/poll.controller.js
+++ controllers/poll.controller.js
@@ -3,6 +3,10 @@ const { createPollDB } = require("../services/poll.service");
 exports.createPoll = async (req, res) => {
   try {
     const result = await createPollDB(req.body);
+    res.status(200).json({
+      status: "success",
+      message: "Successfully Create ",
+    });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
